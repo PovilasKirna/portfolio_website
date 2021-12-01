b6805f31c4cca7999776743e22c82dfdaf592f0f
Povilas Kirna <povilas.kirna@gmail.com>
2021-12-01 11:42:30 +0100

    added favicons

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,6 +9,47 @@ const Main = ({ children, router }) => {
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<title> Povilas Kirna - Homepage</title>
+				<link
+					rel="icon"
+					type="image/png"
+					href="images/favicons/touch-icon-16x16.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					href="images/favicons/touch-icon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					href="images/favicons/touch-icon-48x48.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="60x60"
+					href="images/favicons/apple-touch-icon-60x60.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="76x76"
+					href="images/favicons/apple-touch-icon-76x76.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="120x120"
+					href="images/favicons/apple-touch-icon-120x120.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="152x152"
+					href="images/favicons/apple-touch-icon-152x152.png"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="images/favicons/apple-touch-icon-180x180.png"
+				/>
+				<link rel="manifest" href="/manifest.json" />
 			</Head>
 			<Navbar path={router.asPath} />
 
