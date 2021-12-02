1656dd4d48cae0df4b60b39c072e4a66ae9c1134
Povilas Kirna <povilas.kirna@gmail.com>
2021-12-02 22:18:12 +0100

    Adding posts page

--- pages/posts.js
+++ pages/posts.js
@@ -2,15 +2,22 @@ import NextLink from "next/link";
 import { useColorModeValue } from "@chakra-ui/color-mode";
 import {
 	Heading,
-	Box,
-	Text,
+	SimpleGrid,
 	Container,
 	Divider,
 	Button,
+	Tab,
+	Tabs,
+	TabList,
+	TabPanels,
+	TabPanel,
 } from "@chakra-ui/react";
 import Section from "../components/section";
 import Head from "next/head";
 import Layout from "../components/layouts/article";
+import { WorkGridItem } from "../components/grid-item";
+import thumbSpotifyDisplay from "../public/images/works/thumbSpotifyDisplay.png"; //done
+import thumbEcaTwitterDashboard from "../public/images/works/thumbTwitterDashboard.png"; //done
 
 const Posts = () => {
 	return (
@@ -23,15 +30,65 @@ const Posts = () => {
 					Posts
 				</Heading>
 				<Section delay={0.1}>
-					<Text>The page you&apos;re looking is under development.</Text>
-					<Divider my={6} />
-					<Box my={6} align="center">
-						<NextLink href="/">
-							<Button colorScheme={useColorModeValue("purple", "orange")}>
-								Return to Homepage
-							</Button>
-						</NextLink>
-					</Box>
+					<Tabs isFitted variant="unstyled">
+						<TabList mb="1em">
+							<Tab
+								style={{ borderRadius: 10 }}
+								_selected={{
+									color: `${useColorModeValue("whiteAlpha.900", "gray200")}`,
+									bg: `${useColorModeValue("purple.500", "orange.200")}`,
+									borderRadius: 10,
+									marginRight: 6,
+								}}
+								_hover={{
+									color: `${useColorModeValue("whiteAlpha.900", "gray200")}`,
+									bg: `${useColorModeValue("purple.500", "orange.200")}`,
+									borderRadius: 10,
+								}}
+							>
+								Youtube
+							</Tab>
+							<Tab
+								style={{ borderRadius: 10 }}
+								_selected={{
+									color: `${useColorModeValue("whiteAlpha.900", "gray200")}`,
+									bg: `${useColorModeValue("purple.500", "orange.200")}`,
+									borderRadius: 10,
+									marginLeft: 6,
+								}}
+								_hover={{
+									color: `${useColorModeValue("whiteAlpha.900", "gray200")}`,
+									bg: `${useColorModeValue("purple.500", "orange.200")}`,
+									borderRadius: 10,
+								}}
+							>
+								Blog Post
+							</Tab>
+						</TabList>
+						<TabPanels>
+							<TabPanel>
+								<SimpleGrid columns={[1, 1, 2]} gap={6}>
+									<Section delay={0.1}>
+										<WorkGridItem
+											id="spotifyDisplay"
+											title="Spotify Display"
+											thumbnail={thumbSpotifyDisplay}
+										></WorkGridItem>
+									</Section>
+									<Section delay={0.1}>
+										<WorkGridItem
+											id="ecaTwitterDashboard"
+											title="CrimeBook - Twitter Dashboard"
+											thumbnail={thumbEcaTwitterDashboard}
+										></WorkGridItem>
+									</Section>
+								</SimpleGrid>
+							</TabPanel>
+							<TabPanel>
+								<p>two!</p>
+							</TabPanel>
+						</TabPanels>
+					</Tabs>
 				</Section>
 			</Container>
 		</Layout>
